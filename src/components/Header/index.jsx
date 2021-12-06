4d2737e32fe877ea961f6db6868319b47467fa46
Jonathan Fillipe Lemes <jlemes@findmyprofession.com>
2021-12-06 14:56:25 -0300

    style: 💄 header style changed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,8 +6,10 @@ import { ReactComponent as LogoCdc } from '../../assets/img/logo-cdc.svg';
 export default function Header() {
     return (
         <div className="header-container">
-            <LogoCdc className="logo" />
-            Header
+            <div className="header">
+                <LogoCdc className="logo" />
+                Header
+            </div>
         </div>
     )
 }
