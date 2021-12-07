7144f8044f45c428bc5c73bd9100dd396ac8e025
Jonathan Fillipe Lemes <jlemes@findmyprofession.com>
2021-12-06 21:14:11 -0300

    feat: 🎸 part one of landing page finished

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,7 +7,15 @@ export default function Home() {
     return (
         <div className="home-container">
             <Header />
-            Home
+            <div className="page no-one">
+                <div className="content">
+                    <h1>O MAIOR CLUBE DE ASSINATURA DE CACHAÇA</h1>
+                    <h3>Receba todo mês no conforto de sua casa as melhores cachaças do Brasil. Já são mais de 5 mil assinantes por todo o país!</h3>
+                    <div className="btn-container">
+                        <button className="btn btn-primary">CONHEÇA O CLUBE!</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
