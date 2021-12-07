359ad1c674b5f901abfa5d6ed8b8922ffb30db07
Jonathan Fillipe Lemes <jlemes@findmyprofession.com>
2021-12-06 21:51:32 -0300

    style: 💄 google fonts added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -9,8 +9,10 @@ export default function Home() {
             <Header />
             <div className="page no-one">
                 <div className="content">
-                    <h1>O MAIOR CLUBE DE ASSINATURA DE CACHAÇA</h1>
-                    <h3>Receba todo mês no conforto de sua casa as melhores cachaças do Brasil. Já são mais de 5 mil assinantes por todo o país!</h3>
+                    <div>
+                        <h1>O MAIOR CLUBE DE ASSINATURA DE CACHAÇA</h1>
+                        <h3>Receba todo mês no conforto de sua casa as melhores cachaças do Brasil. Já são mais de 5 mil assinantes por todo o país!</h3>
+                    </div>
                     <div className="btn-container">
                         <button className="btn btn-primary">CONHEÇA O CLUBE!</button>
                     </div>
