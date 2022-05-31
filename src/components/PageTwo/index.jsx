afb94f27b684d7f26864f26147dba786c40d0818
jjpdr <joaop.marcelino@hotmail.com>
2022-05-31 15:15:27 -0300

    style: 💄 fixed styling on pages two and three

--- src/components/PageTwo/index.jsx
+++ src/components/PageTwo/index.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./style.scss";
 import api from "../../services/api";
-import arrow from "../../assets/img/arrow.png";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 export default function PageTwo() {
     const [products, setProducts] = useState([]);
-    const carousel = useRef(null);
 
     useEffect(() => {
         api.get("/products")
@@ -20,52 +18,37 @@ export default function PageTwo() {
         // eslint-disable-next-line
     }, []);
 
-    const handleLeftClick = (event) => {
-        carousel.current.scrollLeft -= carousel.current.offsetWidth;
-    };
-
-    const handleRightClick = (event) => {
-        carousel.current.scrollLeft += carousel.current.offsetWidth;
-    };
-
     return (
         <>
             {products.length > 0 && (
                 <div id="page-two" className="page-container-page-two">
                     <div className="content">
                         <h2>CONHEÇA ALGUNS DE NOSSOS PRODUTOS</h2>
-                        <div className="product" ref={carousel}>
-                            {products.map((product) => {
-                                console.log(product)
-                                return (
-                                    <Link
-                                        to={`/product/${product._id}`}
-                                        style={{ textDecoration: "none" }}
-                                    >
-                                        <div className="product-items">
-                                            <img
-                                                src={`${BACKEND_URL}/products/image/${product.image}`}
-                                                alt="produto"
-                                            />
-                                            <h2 className="name-font">
-                                                {product.name}
-                                            </h2>
-                                            <h2 className="price">
-                                                R${product.price.toFixed(2)}
-                                            </h2>
-                                        </div>
-                                    </Link>
-                                );
+                        <div className="product">
+                            {products.map((product, index) => {
+                                if (index < 3) {
+                                    return (
+                                        <Link
+                                            to={`/product/${product._id}`}
+                                            style={{ textDecoration: "none" }}
+                                        >
+                                            <div className="product-items">
+                                                <img
+                                                    src={`${BACKEND_URL}/products/image/${product.image}`}
+                                                    alt="produto"
+                                                />
+                                                <h2 className="name-font">
+                                                    {product.name}
+                                                </h2>
+                                                <h2 className="price">
+                                                    R${product.price.toFixed(2)}
+                                                </h2>
+                                            </div>
+                                        </Link>
+                                    );
+                                }
                             })}
                         </div>
-                        {/* <div className="buttons">
-                            <button onClick={handleLeftClick}>
-                                <img src={arrow} alt="Left arrow" />
-                            </button>
-                            <button onClick={handleRightClick}>
-                                <img src={arrow} alt="Right arrow" />
-                            </button>
-                        </div> */}
                     </div>
                 </div>
             )}
