a23b6115fc4f34104c247fe22462f9f1af3de2f2
jjpdr <joaop.marcelino@hotmail.com>
2022-05-31 19:38:43 -0300

    feat: 🎸 added a price filter in catalog page

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -10,45 +10,115 @@ import api from "../../services/api";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 export default function Catalog() {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [filterProducts, setFilterProducts] = useState([]);
+    const [sliderPrice, setSliderPrice] = useState("10");
+    const [checkboxBrand, setCheckboxBrand] = useState([]);
 
-  useEffect(() => {
-    api
-      .get("/products")
-      .then((res) => {
-        setProducts([...res.data.products, ...res.data.products]);
-      })
-      .catch((err) => {});
-    // eslint-disable-next-line
-  }, []);
+    const handleChange = (value, field) => {
+        switch (field) {
+            case "price":
+                setSliderPrice(value);
+                console.log(sliderPrice);
+                break;
+        }
+    };
 
-  return (
-    <>
-      <Header />
-      <div className="page-container-catalog">
-        <div className="catalog-sidebar"></div>
-        <div className="flex-catalog-product-list">
-          <div className="catalog-product-list">
-            {products.map((product) => {
-              console.log(product);
-              return (
-                <Link
-                  to={`/product/${product._id}`}
-                  style={{ textDecoration: "none" }}
-                  className="product-items"
-                >
-                  <img
-                    src={`${BACKEND_URL}/products/image/${product.image}`}
-                    alt="produto"
-                  />
-                  <h2>{product.name}</h2>
-                  <h2>R${product.price.toFixed(2)}</h2>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    const sliderFilter = (product) => {
+        if (product.price <= sliderPrice) return product;
+    };
+
+    useEffect(() => {
+        setFilterProducts(products.filter(sliderFilter));
+    }, [sliderPrice]);
+
+    useEffect(() => {
+        api.get("/products")
+            .then((res) => {
+                setProducts([...res.data.products, ...res.data.products]);
+                setFilterProducts(products);
+            })
+            .catch((err) => {});
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <div className="page-container-catalog">
+                <div className="catalog-sidebar">
+                    <h2>FILTRAR POR:</h2>
+                    <div className="catalog-sidebar-item">
+                        <input
+                            type="range"
+                            min="10"
+                            max="100"
+                            step="1"
+                            value={sliderPrice}
+                            className="slider"
+                            onChange={(event) =>
+                                handleChange(event.target.value, "price")
+                            }
+                        ></input>
+                    </div>
+                    <div className="catalog-sidebar-item">
+                        <input
+                            type="checkbox"
+                            value={checkboxBrand}
+                            className="checkbox"
+                            id="Cachaça 51"
+                        ></input>
+                        <label for="Cachaça 51">Cachaça 51</label>
+                    </div>
+                    <div className="catalog-sidebar-item">
+                        <input
+                            type="checkbox"
+                            value={checkboxBrand}
+                            className="checkbox"
+                            id="Velho Barreiro"
+                        ></input>
+                        <label for="Velho Barreiro">Velho Barreiro</label>
+                    </div>
+                    <div className="catalog-sidebar-item">
+                        <input
+                            type="checkbox"
+                            value={checkboxBrand}
+                            className="checkbox"
+                            id="Pinga Azul"
+                        ></input>
+                        <label for="Pinga Azul">Pinga Azul</label>
+                    </div>
+                    <div className="catalog-sidebar-item">
+                        <input
+                            type="checkbox"
+                            value={checkboxBrand}
+                            className="checkbox"
+                            id="Outros"
+                        ></input>
+                        <label for="Outros">Outros</label>
+                    </div>
+                </div>
+                <div className="flex-catalog-product-list">
+                    <div className="catalog-product-list">
+                        {filterProducts.map((product) => {
+                            return (
+                                <Link
+                                    to={`/product/${product._id}`}
+                                    style={{ textDecoration: "none" }}
+                                    className="product-items"
+                                >
+                                    <img
+                                        src={`${BACKEND_URL}/products/image/${product.image}`}
+                                        alt="produto"
+                                    />
+                                    <h2>{product.name}</h2>
+                                    <h2>R${product.price.toFixed(2)}</h2>
+                                </Link>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
