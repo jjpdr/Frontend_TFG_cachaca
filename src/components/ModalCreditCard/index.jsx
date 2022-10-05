019e31982c7c28f0c74e0b525472e83af32c320e
jjpdr <joaop.marcelino@hotmail.com>
2022-10-05 11:47:23 -0300

    feat: 🎸 user's payment methods now show in user's page

--- src/components/ModalCreditCard/index.jsx
+++ src/components/ModalCreditCard/index.jsx
@@ -7,67 +7,66 @@ import { useParams } from "react-router-dom";
 import api from "../../services/api";
 
 export default function ModalCreditCard(props) {
-  const token = JSON.parse(localStorage.getItem("token"));
-  const { id } = useParams();
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [cvc, setCvc] = useState("");
-  const [expiry, setExpiry] = useState("");
+    const token = JSON.parse(localStorage.getItem("token"));
+    const { id } = useParams();
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+    const [cvc, setCvc] = useState("");
+    const [expiry, setExpiry] = useState("");
 
-  const handleSubmit = async () => {
-    const data = {
-      name,
-      number,
-      cvc,
-      expiry,
+    const handleSubmit = async () => {
+        const data = {
+            name,
+            number,
+            cvc,
+            expiry,
+        };
+
+        api.post(`/users/payment-method/${id}`, data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => {
+                alert("Cadastro efetuado com sucesso!");
+                props.onHide();
+                // window.location.reload();
+            })
+            .catch((err) => {
+                alert(err.response.data.message);
+                // window.location.reload();
+            });
     };
 
-    api
-      .post(`/users/payment-method/${id}`, data, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        alert("Cadastro efetuado com sucesso!");
-        props.onHide();
-        // window.location.reload();
-      })
-      .catch((err) => {
-        alert(err.response.data.message);
-        // window.location.reload();
-      });
-  };
-
-  return (
-    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Adicionar novo método de pagamento
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="show-grid">
-        <Container>
-          <PaymentMethod
-            cvc={cvc}
-            setCvc={setCvc}
-            expiry={expiry}
-            setExpiry={setExpiry}
-            name={name}
-            setName={setName}
-            number={number}
-            setNumber={setNumber}
-          />
-        </Container>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="dark" onClick={handleSubmit}>
-          Aceitar
-        </Button>
-        <Button variant="ligth" onClick={props.onHide}>
-          Fechar
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    return (
+        <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Adicionar novo método de pagamento
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="show-grid">
+                <Container>
+                    <PaymentMethod
+                        cvc={cvc}
+                        setCvc={setCvc}
+                        expiry={expiry}
+                        setExpiry={setExpiry}
+                        name={name}
+                        setName={setName}
+                        number={number}
+                        setNumber={setNumber}
+                    />
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="dark" onClick={handleSubmit}>
+                    Aceitar
+                </Button>
+                <Button variant="ligth" onClick={props.onHide}>
+                    Fechar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
--- src/components/PaymentMethod/index.jsx
+++ src/components/PaymentMethod/index.jsx
@@ -3,104 +3,104 @@ import Cards from "react-credit-cards";
 import "react-credit-cards/lib/styles.scss";
 import ReactInputDateMask from "react-input-date-mask";
 import {
-  MDBBtn,
-  MDBCol,
-  MDBContainer,
-  MDBInput,
-  MDBRow,
-  MDBFile,
+    MDBBtn,
+    MDBCol,
+    MDBContainer,
+    MDBInput,
+    MDBRow,
+    MDBFile,
 } from "mdb-react-ui-kit";
 
 export default function PaymentForm({
-  cvc,
-  setCvc,
-  expiry,
-  setExpiry,
-  name,
-  setName,
-  number,
-  setNumber,
+    cvc,
+    setCvc,
+    expiry,
+    setExpiry,
+    name,
+    setName,
+    number,
+    setNumber,
 }) {
-  const [focus, setFocus] = useState(false);
+    const [focus, setFocus] = useState(false);
 
-  const handleInputFocus = (e) => {
-    setFocus(e.target.name);
-  };
+    const handleInputFocus = (e) => {
+        setFocus(e.target.name);
+    };
 
-  const handleInputChange = (e, name) => {
-    switch (name) {
-      case "cvc":
-        setCvc(e.target.value);
-        break;
-      case "expiry":
-        setExpiry(e.target.value);
-        break;
-      case "name":
-        setName(e.target.value);
-        break;
-      case "number":
-        setNumber(e.target.value);
-        break;
-    }
-  };
+    const handleInputChange = (e, name) => {
+        switch (name) {
+            case "cvc":
+                setCvc(e.target.value);
+                break;
+            case "expiry":
+                setExpiry(e.target.value);
+                break;
+            case "name":
+                setName(e.target.value.toUpperCase());
+                break;
+            case "number":
+                setNumber(e.target.value);
+                break;
+        }
+    };
 
-  return (
-    <div id="PaymentForm">
-      <Cards
-        cvc={cvc}
-        expiry={expiry}
-        focused={focus}
-        name={name}
-        number={number}
-      />
-      <MDBContainer>
-        <MDBRow style={{ marginTop: "10px" }}>
-          <MDBInput
-            type="tel"
-            name="number"
-            onChange={(e) => handleInputChange(e, "number")}
-            onFocus={handleInputFocus}
-            label="Número do cartão"
-            maxLength={16}
-          />
-        </MDBRow>
-        <MDBRow style={{ marginTop: "10px" }}>
-          <MDBInput
-            type="text"
-            name="name"
-            label="Nome"
-            onChange={(e) => handleInputChange(e, "name")}
-            onFocus={handleInputFocus}
-            maxLength={30}
-          />
-        </MDBRow>
-        <MDBRow
-          style={{
-            marginTop: "10px",
-          }}
-        >
-          <MDBCol style={{ padding: "0px" }}>
-            <MDBInput
-              type="tel"
-              name="expiry"
-              label="Validade"
-              onChange={(e) => handleInputChange(e, "expiry")}
-              onFocus={handleInputFocus}
-              maxLength={4}
+    return (
+        <div id="PaymentForm">
+            <Cards
+                cvc={cvc}
+                expiry={expiry}
+                focused={focus}
+                name={name}
+                number={number}
             />
-          </MDBCol>
-          <MDBCol style={{ paddingRight: "0px" }}>
-            <MDBInput
-              type="tel"
-              name="cvc"
-              label="CVC"
-              onChange={(e) => handleInputChange(e, "cvc")}
-              onFocus={handleInputFocus}
-              maxLength={3}
-            />
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </div>
-  );
+            <MDBContainer>
+                <MDBRow style={{ marginTop: "10px" }}>
+                    <MDBInput
+                        type="tel"
+                        name="number"
+                        onChange={(e) => handleInputChange(e, "number")}
+                        onFocus={handleInputFocus}
+                        label="Número do cartão"
+                        maxLength={16}
+                    />
+                </MDBRow>
+                <MDBRow style={{ marginTop: "10px" }}>
+                    <MDBInput
+                        type="text"
+                        name="name"
+                        label="Nome"
+                        onChange={(e) => handleInputChange(e, "name")}
+                        onFocus={handleInputFocus}
+                        maxLength={30}
+                    />
+                </MDBRow>
+                <MDBRow
+                    style={{
+                        marginTop: "10px",
+                    }}
+                >
+                    <MDBCol style={{ padding: "0px" }}>
+                        <MDBInput
+                            type="tel"
+                            name="expiry"
+                            label="Validade"
+                            onChange={(e) => handleInputChange(e, "expiry")}
+                            onFocus={handleInputFocus}
+                            maxLength={4}
+                        />
+                    </MDBCol>
+                    <MDBCol style={{ paddingRight: "0px" }}>
+                        <MDBInput
+                            type="tel"
+                            name="cvc"
+                            label="CVC"
+                            onChange={(e) => handleInputChange(e, "cvc")}
+                            onFocus={handleInputFocus}
+                            maxLength={3}
+                        />
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </div>
+    );
 }
